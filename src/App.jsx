632e57391e242735e82b4b4f1ab252dbbf20a3f8
Guilherme Zaparoli <guilhermezapas@outlook.com>
2023-05-17 11:47:17 -0300

    Header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { Header } from "./Components/Header";
-import { Post } from "./Post";
+import { Post } from "./Components/Post";
+import styles from "./App.module.css";
+import { Sidebar } from "./Components/Sidebar";
+
 import "./styles.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure expedita enim placeat pariatur consequatur, repellendus reprehenderit? Mollitia modi reprehenderit dolor in! A, est. Quasi, autem cupiditate? Laboriosam, eius obcaecati."
-      />
 
-      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure expedita enim placeat pariatur consequatur, repellendus reprehenderit? Mollitia modi reprehenderit dolor in! A, est. Quasi, autem cupiditate? Laboriosam, eius obcaecati."
+          />
+
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   );
 }
